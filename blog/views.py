71e3002fadb136ea5f88cd043e9ea470cebe22a7
Jean Charles Severino <f23severino@gmail.com>
2020-09-19 00:17:53 -0300

    Projeto Django 2 - versão 1.3

---
+++ blog/views.py
@@ -0,0 +1,289 @@
+from django.shortcuts import render
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+
+def historico1(request):
+    return render(request, 'blog/historico1.html', {})
+
+def index(request):
+    return render(request, 'index.html', {})
+
+def militares(request):
+    return render(request, 'blog/militares.html', {})
+
+def missao(request):
+    return render(request, 'blog/missao.html', {})
+
+def objetivos(request):
+    return render(request, 'blog/objetivos.html', {})
+
+def telefones(request):
+    return render(request, 'blog/telefones.html', {})
+
+def telefones2(request):
+    return render(request, 'blog/telefones2.html', {})
+
+def visao(request):
+    return render(request, 'blog/visao.html', {})
+
+def avisos(request):
+    return render(request, 'blog/avisos.html', {})
+
+def diretor(request):
+    return render(request, 'blog/diretor.html', {})
+
+def subdiretor(request):
+    return render(request, 'blog/subdiretor.html', {})
+
+def exdiretores(request):
+    return render(request, 'blog/exdiretores.html', {})
+
+def inteligencia(request):
+    return render(request, 'blog/inteligencia.html', {})
+
+def relacoespublicas(request):
+    return render(request, 'blog/relacoespublicas.html', {})
+
+def administracao(request):
+    return render(request, 'blog/administracao.html', {})
+
+def instrucao(request):
+    return render(request, 'blog/instrucao.html', {})
+
+def aniversarios1(request):
+    return render(request, 'blog/aniversarios1.html', {})
+
+def aniversarios11(request):
+    return render(request, 'blog/aniversarios11.html', {})
+
+def aniversarios12(request):
+    return render(request, 'blog/aniversarios12.html', {})
+
+def aniversarios13(request):
+    return render(request, 'blog/aniversarios13.html', {})
+
+def aniversarios14(request):
+    return render(request, 'blog/aniversarios14.html', {})
+
+def aniversarios15(request):
+    return render(request, 'blog/aniversarios15.html', {})
+
+def aniversarios16(request):
+    return render(request, 'blog/aniversarios16.html', {})
+
+def aniversarios17(request):
+    return render(request, 'blog/aniversarios17.html', {})
+
+def aniversarios18(request):
+    return render(request, 'blog/aniversarios18.html', {})
+
+def aniversarios19(request):
+    return render(request, 'blog/aniversarios19.html', {})
+
+def aniversarios110(request):
+    return render(request, 'blog/aniversarios110.html', {})
+
+def aniversarios111(request):
+    return render(request, 'blog/aniversarios112.html', {})
+
+def aniversarios112(request):
+    return render(request, 'blog/aniversarios112.html', {})
+
+def bi20151(request):
+    return render(request, 'blog/bi20151.html', {})
+
+def bi201511(request):
+    return render(request, 'blog/bi201511.html', {})
+
+def bi201512(request):
+    return render(request, 'blog/bi201512.html', {})
+
+def bi201513(request):
+    return render(request, 'blog/bi201513.html', {})
+
+def bi201514(request):
+    return render(request, 'blog/bi201514.html', {})
+
+def bi201515(request):
+    return render(request, 'blog/bi201515.html', {})
+
+def bi201516(request):
+    return render(request, 'blog/bi201516.html', {})
+
+def bi201517(request):
+    return render(request, 'blog/bi201517.html', {})
+
+def bi201518(request):
+    return render(request, 'blog/bi201518.html', {})
+
+def bi201519(request):
+    return render(request, 'blog/bi201519.html', {})
+
+def bi2015110(request):
+    return render(request, 'blog/bi2015110.html', {})
+
+def bi2015111(request):
+    return render(request, 'blog/bi2015111.html', {})
+
+def bi2015112(request):
+    return render(request, 'blog/bi2015112.html', {})
+
+def bi20161(request):
+    return render(request, 'blog/bi20161.html', {})
+
+def bi201611(request):
+    return render(request, 'blog/bi201611.html', {})
+
+def bi201612(request):
+    return render(request, 'blog/bi201612.html', {})
+
+def bi201613(request):
+    return render(request, 'blog/bi201613.html', {})
+
+def bi201614(request):
+    return render(request, 'blog/bi201614.html', {})
+
+def bi201615(request):
+    return render(request, 'blog/bi201615.html', {})
+
+def bi201616(request):
+    return render(request, 'blog/bi201616.html', {})
+
+def bi201617(request):
+    return render(request, 'blog/bi201617.html', {})
+
+def bi201618(request):
+    return render(request, 'blog/bi201618.html', {})
+
+def bi201619(request):
+    return render(request, 'blog/bi201619.html', {})
+
+def bi2016110(request):
+    return render(request, 'blog/bi2016110.html', {})
+
+def bi2016111(request):
+    return render(request, 'blog/bi2016111.html', {})
+
+def bi2016112(request):
+    return render(request, 'blog/bi2016112.html', {})
+
+def bi20171(request):
+    return render(request, 'blog/bi20171.html', {})
+
+def bi201711(request):
+    return render(request, 'blog/bi201711.html', {})
+
+def bi201712(request):
+    return render(request, 'blog/bi201712.html', {})
+
+def bi201713(request):
+    return render(request, 'blog/bi201713.html', {})
+
+def bi201714(request):
+    return render(request, 'blog/bi201714.html', {})
+
+def bi201715(request):
+    return render(request, 'blog/bi201715.html', {})
+
+def bi201716(request):
+    return render(request, 'blog/bi201716.html', {})
+
+def bi201717(request):
+    return render(request, 'blog/bi201717.html', {})
+
+def bi201718(request):
+    return render(request, 'blog/bi201718.html', {})
+
+def bi201719(request):
+    return render(request, 'blog/bi201719.html', {})
+
+def bi2017110(request):
+    return render(request, 'blog/bi2017110.html', {})
+
+def bi2017111(request):
+    return render(request, 'blog/bi2017111.html', {})
+
+def bi2017112(request):
+    return render(request, 'blog/bi2017112.html', {})
+
+def bi20181(request):
+    return render(request, 'blog/bi20181.html', {})
+
+def bi201811(request):
+    return render(request, 'blog/bi201811.html', {})
+
+def bi201812(request):
+    return render(request, 'blog/bi201812.html', {})
+
+def bi201813(request):
+    return render(request, 'blog/bi201813.html', {})
+
+def bi201814(request):
+    return render(request, 'blog/bi201814.html', {})
+
+def bi201815(request):
+    return render(request, 'blog/bi201815.html', {})
+
+def bi201816(request):
+    return render(request, 'blog/bi201816.html', {})
+
+def bi201817(request):
+    return render(request, 'blog/bi201817.html', {})
+
+def bi201818(request):
+    return render(request, 'blog/bi201818.html', {})
+
+def bi201819(request):
+    return render(request, 'blog/bi201819.html', {})
+
+def bi2018110(request):
+    return render(request, 'blog/bi2018110.html', {})
+
+def bi2018111(request):
+    return render(request, 'blog/bi2018111.html', {})
+
+def bi2018112(request):
+    return render(request, 'blog/bi2018112.html', {})
+
+def bi20191(request):
+    return render(request, 'blog/bi20191.html', {})
+
+def bi201911(request):
+    return render(request, 'blog/bi201911.html', {})
+
+def bi201912(request):
+    return render(request, 'blog/bi201912.html', {})
+
+def bi201913(request):
+    return render(request, 'blog/bi201913.html', {})
+
+def bi201914(request):
+    return render(request, 'blog/bi201914.html', {})
+
+def bi201915(request):
+    return render(request, 'blog/bi201915.html', {})
+
+def bi201916(request):
+    return render(request, 'blog/bi201916.html', {})
+
+def bi201917(request):
+    return render(request, 'blog/bi201917.html', {})
+
+def bi201918(request):
+    return render(request, 'blog/bi201918.html', {})
+
+def bi201919(request):
+    return render(request, 'blog/bi201919.html', {})
+
+def bi2019110(request):
+    return render(request, 'blog/bi2019110.html', {})
+
+def bi2019111(request):
+    return render(request, 'blog/bi2019111.html', {})
+
+def bi2019112(request):
+    return render(request, 'blog/bi2019112.html', {})
+
+# def login(request):
+    # return render(request, 'blog/login.html', {})
---
+++ mysite/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', LoginView.as_view(template_name='registration/login.html'), name="login"),
+    # path('', TemplateView.as_view(template_name='index.html'), name='index'),
+]
