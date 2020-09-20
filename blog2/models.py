47e5c44ab4085f65b730b146df34a7c014471c9b
Jean Charles Severino <f23severino@gmail.com>
2020-09-19 21:58:57 -0300

    Projeto Django 2 - versão 1.4

--- blog2/models.py
+++ blog2/models.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class PostAvisos(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth.views import LoginView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('forumAvisos', include('blog2.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', LoginView.as_view(template_name='registration/login.html'), name="login"),
     # path('', TemplateView.as_view(template_name='index.html'), name='index'),
